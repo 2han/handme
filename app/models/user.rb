4f5e6cdbfd6c39fbc8a16844d35642a210b33dbc
2han <junichiito@itoujunichi-no-MacBook-Air.local>
2014-11-25 23:29:35 +0900

    モデルのuser.rbをいじって画像をアップできるようにしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
   has_many :jobs
+
+
+  def set_image(file)
+      if !file.nil?
+        file_name = file.original_filename
+        File.open("public/docs/#{file_name}", 'wb'){|f| f.write(file.read)}
+        self.image = file_name
+      end
+  end
+
 end
