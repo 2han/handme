f590e55206c229f5ec0920fd2a2420701d758763
2han <junichiito@itoujunichi-no-MacBook-Air.local>
2014-11-25 23:21:38 +0900

    user.rbとjob.rbを関連付けた後バリデも少しやりました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+  has_many :jobs
 end
---
+++ db/migrate/20141125140334_add_index_to_users_email.rb
@@ -0,0 +1,5 @@
+class AddTopToUsersEmail < ActiveRecord::Migration
+  def change
+    add_index :users, :email, unique: true
+  end
+end
